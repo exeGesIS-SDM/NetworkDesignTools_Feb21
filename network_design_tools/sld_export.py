e2317a97ad0a5bcab019311c30330b52d1a506e9
ClaireLush <clairelush@esdm.co.uk>
2021-05-12 17:20:33 +0100

    #7 Reverting to orthogonal lines as per client feedback, expanding diagram and reformatting node labels

--- network_design_tools/sld_export.py
+++ network_design_tools/sld_export.py
@@ -46,7 +46,7 @@ def createSLD(iface, bdry_lyr):
     request = QgsFeatureRequest()
 
     # Set to draw from left to right
-    g.attr(rankdir='LR', splines='line')
+    g.attr(rankdir='LR', splines='ortho', ranksep='4', pad='0.25')
     g.attr('edge', fontsize='12', fontname='arial')
     g.attr('node', fontsize='12', fontname='arial')
 
@@ -55,104 +55,80 @@ def createSLD(iface, bdry_lyr):
     nodes_lyr.selectByExpression('\"{}\" = 1'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
     for node in nodes_lyr.getSelectedFeatures():
         node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}P<br />{}</b>>'.format(node_id, node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
+        node_label = []
+        node_label.append('<<b><table border="0" cellpadding="0" cellspacing="0">')
+        node_label.append('<tr><td align="left">{}</td></tr>'.format(node_id))
+        node_label.append('<tr><td align="left">{}P</td></tr>'.format(node[node_flds['premises']]))
+        node_label.append('<tr><td align="left">{}</td></tr>'.format('</td></tr><tr><td align="left">'.join(wrap(node[node_flds['address']], width=25))))
+        node_label.append('</table></b>>')
         with g.subgraph(name='cluster_{}'.format(node_id)) as c:
             c.attr(style='invis')
             c.node(node_id, label='UGPN', shape='square', width='1.5', height='1.5', fixedsize='shape')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
+            c.node('{}_L_html'.format(node_id), label=''.join(node_label), shape='plain')
 
-    # Add secondary nodes
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 2'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='UGSN', shape='square', width='1', height='1', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id).format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 3'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='PMSN', shape='circle', width='1', height='1', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 6'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='MSN', shape='triangle', width='1.1', height='1.1', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-    # Add tertiary nodes
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 5'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='UGCE', shape='square', width='0.7', height='0.7', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 4'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='PMCE', shape='circle', width='0.7', height='0.7', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-
-    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
-    nodes_lyr.selectByExpression('\"{}\" = 7'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
-    for node in nodes_lyr.getSelectedFeatures():
-        node_id = node[node_flds['id']]
-        node_label = '<<b>{}<br />{}x1:8 ({}P)<br />{}</b>>'.format(node_id, node[node_flds['splitters_1_8']], node[node_flds['premises']], \
-            '<br />'.join(wrap(node[node_flds['address']], width=25)))
-        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
-            c.attr(style='invis')
-            c.node(node_id, label='MCE', shape='triangle', width='0.8', height='0.8', fixedsize='shape')
-            c.node('{}_F_html'.format(node_id), label='<<b>{}F</b>>'.format(node[node_flds['splitters_1_8']]), shape='plain', fontcolor='Red')
-            c.node('{}_L_html'.format(node_id), label=node_label, shape='plain')
-
-    nodes_lyr.removeSelection()
-
-    # Add joints to nodes
+    # Add joints
     joint_flds = layers['Joint']['fields']
 
     processing.run("qgis:selectbylocation", { 'INPUT' : joints_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
     for joint in joints_lyr.getSelectedFeatures():
         joint_id = joint[joint_flds['id']]
-        joint_label = '<<b>{}<br />{}<br />{}</b>>'.format(joint_id, joint[joint_flds['size']], '<br />'.join(wrap(joint[joint_flds['address']], width=25)))
+        joint_label = []
+        joint_label.append('<<b><table border="0" cellpadding="0" cellspacing="0">')
+        joint_label.append('<tr><td align="left">{}</td></tr>'.format(joint_id))
+        joint_label.append('<tr><td align="left">{}</td></tr>'.format(joint[joint_flds['size']]))
+        joint_label.append('<tr><td align="left">{}</td></tr>'.format('</td></tr><tr><td align="left">'.join(wrap(joint[joint_flds['address']], width=25))))
+        joint_label.append('</table></b>>')
         with g.subgraph(name='cluster_{}'.format(joint[joint_flds['id']])) as c:
             c.attr(style='invis')
             with c.subgraph(name='cluster_{}_S'.format(joint[joint_flds['id']])) as s:
                 s.attr(style='dashed', margin='25')
                 s.node(joint_id, label='', shape='circle', style='filled', color='black', width='0.3', height='0.3', fixedsize='shape')
-            c.node('{}_L_html'.format(joint_id), label=joint_label, shape='plain')
+            c.node('{}_L_html'.format(joint_id), label=''.join(joint_label), shape='plain')
+
+    # Add secondary nodes
+    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
+    nodes_lyr.selectByExpression('\"{}\" IN (2, 5, 6, 7)'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
+    for node in nodes_lyr.getSelectedFeatures():
+        node_id = node[node_flds['id']]
+        node_label = []
+        node_label.append('<<b><table border="0" cellpadding="0" cellspacing="0">')
+        node_label.append('<tr><td align="left"><font color="red">{}F</font></td></tr>'.format(node[node_flds['splitters_1_8']]))
+        node_label.append('<tr><td align="left">{}</td></tr>'.format(node_id))
+        node_label.append('<tr><td align="left">{}x1:8 ({}P)</td></tr>'.format(node[node_flds['splitters_1_8']], node[node_flds['premises']]))
+        node_label.append('<tr><td align="left">{}</td></tr>'.format('</td></tr><tr><td align="left">'.join(wrap(node[node_flds['address']], width=25))))
+        node_label.append('</table></b>>')
+        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
+            c.attr(style='invis', nodesep='0.02')
+            if node[node_flds['use']] == 2:
+                c.node(node_id, label='UGSN', shape='square', width='1', height='1', fixedsize='shape')
+            elif node[node_flds['use']] == 5:
+                c.node(node_id, label='UGCE', shape='square', width='1', height='1', fixedsize='shape')
+            elif node[node_flds['use']] == 6:
+                c.node(node_id, label='MSN', shape='square', width='1', height='1', fixedsize='shape')
+            else:
+                c.node(node_id, label='MCE', shape='square', width='1', height='1', fixedsize='shape')
+            c.node('{}_L_html'.format(node_id), label=''.join(node_label), shape='plain')
+
+    processing.run("qgis:selectbylocation", { 'INPUT' : nodes_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
+    nodes_lyr.selectByExpression('\"{}\" in (3, 4)'.format(node_flds['use']), QgsVectorLayer.IntersectSelection)
+    for node in nodes_lyr.getSelectedFeatures():
+        node_id = node[node_flds['id']]
+        node_label = []
+        node_label.append('<<b><table border="0" cellpadding="0" cellspacing="0">')
+        node_label.append('<tr><td align="left"><font color="red">{}F</font></td></tr>'.format(node[node_flds['splitters_1_8']]))
+        node_label.append('<tr><td align="left">{}</td></tr>'.format(node_id))
+        node_label.append('<tr><td align="left">{}x1:8 ({}P)</td></tr>'.format(node[node_flds['splitters_1_8']], node[node_flds['premises']]))
+        node_label.append('<tr><td align="left">{}</td></tr>'.format('</td></tr><tr><td align="left">'.join(wrap(node[node_flds['address']], width=25))))
+        node_label.append('</table></b>>')
+        with g.subgraph(name='cluster_{}'.format(node_id)) as c:
+            c.attr(style='invis', nodesep='0.02')
+            if node[node_flds['use']] == 3:
+                c.node(node_id, label='PMSN', shape='circle', width='1', height='1', fixedsize='shape')
+            else:
+                c.node(node_id, label='PMCE', shape='circle', width='1', height='1', fixedsize='shape')
+            c.node('{}_L_html'.format(node_id), label=''.join(node_label), shape='plain')
+
+    nodes_lyr.removeSelection()
 
     cable_flds = layers['Cable']['fields']
     type_index = cable_lyr.dataProvider().fieldNameIndex(cable_flds['type'])
@@ -165,7 +141,7 @@ def createSLD(iface, bdry_lyr):
         label = '{} Cable\n{}'.format(type_label, cable[cable_flds['label']])
         nodes = get_node_ids(cable[cable_flds['name']])
         if len(nodes) == 2:
-            g.edge(nodes[0], nodes[1], label= label)
+            g.edge(nodes[0], nodes[1], xlabel=label)
 
     g.attr('edge', style='dashed')
     processing.run("qgis:selectbylocation", { 'INPUT' : cable_lyr, 'INTERSECT' : bdry_sel_lyr, 'METHOD' : 0, 'PREDICATE' : [0] })
@@ -175,15 +151,15 @@ def createSLD(iface, bdry_lyr):
         label = '{} ULW Cable\n{}'.format(type_label, cable[cable_flds['label']])
         nodes = get_node_ids(cable[cable_flds['name']])
         if len(nodes) == 2:
-            g.edge(nodes[0], nodes[1], label= label)
+            g.edge(nodes[0], nodes[1], xlabel=label)
 
     cable_lyr.removeSelection()
-    try:
-        g.view(cleanup=True)
-    except Exception as e:
-        print(type(e), e)
-        QMessageBox.critical(iface.mainWindow(), "Graphviz missing", "Graphviz executables must be installed and the settings > " + \
-                                                 "graphvizBinPath set to the correct path in the prerequisites.")
+    #try:
+    g.view(cleanup=True)
+    #except Exception as e:
+    #    print(type(e), e)
+    #    QMessageBox.critical(iface.mainWindow(), "Graphviz missing", "Graphviz executables must be installed and the settings > " + \
+    #                                             "graphvizBinPath set to the correct path in the prerequisites.")
 
 def get_node_ids(name):
     node_ids = []
